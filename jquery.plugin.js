644aeed02f9015b543712a94a59b5dc35d24b6ba
nltsm <nltsm@yandex.ru>
2016-09-04 00:30:00 +0300

    Update jquery.plugin.js

--- jquery.plugin.js
+++ jquery.plugin.js
@@ -2,24 +2,47 @@
 	var settings = {
 		
 	};
+
+	var priv = {
 	
+	};
+
 	var methods = {
-		init: function(param) {
-			settings = $.extend(settings, param);
-			
-			return this.each(function() {
-				
-			});
+		init: function (param) {
+			settings = $.extend( settings, param );
+
+			return this.each( function () {
+				var $this = $( this ); // current object DOM
+				var slider = new Plugin( $this );
+			} );
 		},
 	};
 
-	$.fn.pluginName = function (param) {
+	/**
+	 * MAIN CONSTRUCTOR
+	 * @param $this
+	 * @constructor
+	 */
+	function Plugin($this) {
+		
+	}
+
+	/**
+	 * 
+	 */
+	Slider.prototype.pluginFunc = function () {
+		
+	};
+
+
+	$.fn.slider = function (param) {
 		if ( methods[param] ) {
 			return methods[param].apply( this, Array.prototype.slice.call( arguments, 1 ) );
 		}
 		else if ( typeof param === 'object' || !param ) {
-			methods.init.apply(this, arguments);
+			methods.init.apply( this, arguments );
 		}
 	};
 
 }( jQuery ));
+
